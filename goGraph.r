78a5c7dd5757129e2fa837b2fad222aef43af8a4
lecorguille <gildas.le.corguille@gmail.com>
2020-07-21 14:34:49 +0200

    goGraph - 1 namespace / page

--- goGraph.r
+++ goGraph.r
@@ -46,39 +46,28 @@ data[is.na(data)]=0
 #  theme(axis.text.x = element_text(angle=-90, hjust=0, vjust=0.5), plot.title = element_text(vjust=1.5), axis.title.x = element_blank(), axis.title.y = element_blank()) +
 #  labs(title = datainfile)
 
-gg = ggplot(data, aes(x=reorder(GO,-(count.x+count.y)))) +
-  geom_bar(aes(y=count.x), position="identity",stat="identity", fill=mypalette[1], colour="black") +
-  geom_text(aes(y=count.x,label=count.x),vjust=-0.5, colour=mypalette[1], size=3.5) +
-  facet_grid(. ~ spacename, scales = "free", space = "free") +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle=-90, hjust=0, vjust=0.5), plot.title = element_text(vjust=1.5), axis.title.x = element_blank(), axis.title.y = element_blank(), panel.grid.major = element_line(colour = "grey")) +
-  labs(title = datainfile)
+if (!display) pdf(file=paste(datainfile,"pdf",sep="."), width=15, height=12)
+for (spacename in unique(data$spacename)) {
+  data_tmp=data[data$spacename==spacename,]
 
-if (sum(data$count.y) < 0) {
-  gg = gg + geom_bar(aes(y=count.y), position="identity",stat="identity", fill=mypalette[3], colour="black")
-  gg = gg + geom_text(aes(y=count.y,label=(-1*count.y)),vjust=1.5, colour=mypalette[3], size=3.5)
-  gg = gg + expand_limits(y = c(min(data$count.y)-1,max(data$count.x)+1))
-} else {
-  gg = gg + expand_limits(y = c(0,max(data$count.x)+1))
-}
+  gg = ggplot(data_tmp, aes(x=reorder(GO,-(count.x+count.y)))) +
+    geom_bar(aes(y=count.x), position="identity",stat="identity", fill=mypalette[1], colour="black") +
+    geom_text(aes(y=count.x,label=count.x),vjust=-0.5, colour=mypalette[1], size=3.5) +
+    facet_grid(. ~ spacename, scales = "free", space = "free") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle=-90, hjust=0, vjust=0.5), plot.title = element_text(vjust=1.5), axis.title.x = element_blank(), axis.title.y = element_blank(), panel.grid.major = element_line(colour = "grey")) +
+    labs(title = datainfile)
 
-gg
-
-
-
-if (!display) {
-
-  if (nrow(data) <= 10) {
-    scale = 0.75
-  } else if ((10 < nrow(data)) & (nrow(data) <= 25)) {
-    scale = 1
-  } else if ((25 < nrow(data)) & (nrow(data) <= 50)) {
-    scale = 1.5
-  } else if (50 < nrow(data) & (nrow(data) <= 100)) {
-    scale = 2
-  } else if (100 < nrow(data)) {
-    scale = 2.5
+  if (sum(data_tmp$count.y) < 0) {
+    gg = gg + geom_bar(aes(y=count.y), position="identity",stat="identity", fill=mypalette[3], colour="black")
+    gg = gg + geom_text(aes(y=count.y,label=(-1*count.y)),vjust=1.5, colour=mypalette[3], size=3.5)
+    gg = gg + expand_limits(y = c(min(data_tmp$count.y)-1,max(data_tmp$count.x)+1))
+  } else {
+    gg = gg + expand_limits(y = c(0,max(data_tmp$count.x)+1))
   }
-  ggsave(filename=paste(datainfile,"pdf",sep="."), scale=scale)
+
+  print(gg)
 }
+if (!display) dev.off()
+
 
