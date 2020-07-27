958683a79d412da0f1a88665550cdd217348e0a0
lecorguille <gildas.le.corguille@gmail.com>
2020-07-27 16:31:15 +0200

    fix compare - add khi2

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ http://geneontology.org/docs/download-ontology/
 ```
 # Full
 wget http://purl.obolibrary.org/obo/go/go-basic.obo
+#get ftp://ftp.geneontology.org/go/ontology/gene_ontology.obo
 
 # Subsets
 wget http://current.geneontology.org/ontology/subsets/goslim_generic.obo
@@ -18,6 +19,32 @@ wget http://current.geneontology.org/ontology/subsets/goslim_pir.obo
 
 https://docs.conda.io/en/latest/miniconda.html
 ```
-conda create -n goslim-analysis coreutils=8.31 perl-go-perl=0.15 r-ggplot2=3.1.1 r-rcolorbrewer=1.1_2
-source activate goslim-analysis
+conda create -n goslim-analysis coreutils=8.31 perl-go-perl=0.15 r-ggplot2=3.1.1 r-rcolorbrewer=1.1_2 r-optparse
+conda activate goslim-analysis
 ```
+
+## Usage
+### goslimWrapper.sh
+
+#### Inputs
+The mandatory column are: id (here col1), logFC (here col3) and GO numbers separated by space (here col4)
+```
+comp20180_c0_seq1       1.258866547     0.08025349      GO:0005634 GO:0003682 GO:0003677 GO:0005515 GO:0010468 GO:0006355 GO:0006351
+comp2018_c0_seq1        -3.081078296    1.76E-06     GO:0005739 GO:0005634 GO:0043565 GO:0003700 GO:0008270 GO:0007517 GO:0006351
+comp2706_c0_seq1        -7.408903924     8.79E-05        GO:0031225
+comp27949_c0_seq1       -2.251179759    0.000217982  GO:0016021 GO:0005886 GO:0005509 GO:0005515 GO:0021987 GO:0007157 GO:0035329 GO:0007156 GO:0022008
+```
+
+#### Run
+```
+./goslimWrapper.sh table_with_GO_number.tab
+```
+Or something like that if you have plenty of files (here I'm filtering on the filename ended by `U.tab` or `A.tab`)
+```
+conda activate goslim-analysis
+for file_i in *[UA].tab; do echo $file_i; srun "../../../script/goslim-analysis/goslimWrapper.sh $file_i" & echo "next"; done
+```
+
+#### Outputs
+
+### goslimWrapper.sh
--- goGraphComparaison.r
+++ goGraphComparaison.r
@@ -1,90 +1,113 @@
-#!/usr/local/public/bin/Rscript
-wd='/projet/fr2424/sib/lecorguille/15-01-29-Camille-Bathy-GO/tmp/bathy'
-
-datainfile1="Croco_bathy"
-datainfile2="Pop_nat_Bathy"
-
-goslims=c("generic","pir")
-
-display=F
-#display=T
-
-argv <- commandArgs(TRUE)
-if (length(argv) > 0) {
-  datainfile1 = argv[1]
-  datainfile2 = argv[2]
-  goslims=argv[3:length(argv)]
-  display=F
-} else {
-  setwd(wd)
-}
+#!/usr/bin/env Rscript
 
 library(ggplot2)
+library("optparse")
 library(RColorBrewer)
 
 
-for (goslim in goslims) {
-  infile1=paste(datainfile1,".goslim_",goslim,".count.tab",sep="")
-  infile2=paste(datainfile2,".goslim_",goslim,".count.tab",sep="")
+option_list = list(
+  make_option(c("--infile1"), type="character", default=NULL,
+              help="dataset1 filename (ex: subset1.goslim_generic.count.tab)", metavar="character"),
+  make_option(c("--infile2"), type="character", default=NULL,
+              help="dataset2 filename (ex: global.goslim_generic.count.tab)", metavar="character"),
+  make_option(c("--total1"), type="integer", default=NULL,
+              help="total nbr of transcrits in dataset1", metavar="number"),
+  make_option(c("--total2"), type="integer", default=NULL,
+              help="total nbr of transcrits in dataset2", metavar="number"),
+  make_option(c("--shortname1"), type="character", default=NULL,
+              help="a shortname for infile1 (ex: subset1)", metavar="character"),
+  make_option(c("--shortname2"), type="character", default=NULL,
+              help="a shortname for infile2 (ex: global)", metavar="character"),
+  make_option(c("--other_info"), type="character", default=NULL,
+              help="other info that will be added in the output filename and in the graph title (ex: goslim_pir)", metavar="character")
+);
 
-  datafilename=paste(datainfile1,"_vs_",datainfile2,".goslim_",goslim,sep="")
-  datatitle=paste(datainfile1," (red) vs ",datainfile2," (green-normed & white)\nusing goslim_",goslim,sep="")
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
 
-  print(datafilename)
-
-  cond1=read.table(infile1); colnames(cond1) = c("GO","spacename","count")
-  cond2=read.table(infile2); colnames(cond2) = c("GO","spacename","count")
-
-  # normalization on the number of GO in the cond1
-  cond2$count.ynorm=round(cond2$count*sum(cond1$count)/sum(cond2$count))
-
-
-  merged=merge(cond1,cond2,by=c("GO","GO"),all=T)
-  #merged=merged[merged$count.x!=0 & merged$countnorm!=0,] # pour les conditions vs whole
-
-  merged$count.x[is.na(merged$count.x)]=0  # si absent de cond1
-  merged$count.ynorm[is.na(merged$count.ynorm)]=0  # si absent de cond2
-  merged$count.y[is.na(merged$count.y)]=0  # si absent de cond2
-  merged$spacename.y[is.na(merged$spacename.y)]=merged$spacename.x[is.na(merged$spacename.y)] # si absent de cond1
-
-  data=data.frame(
-    "GO"=merged$GO,
-    "spacename"=merged$spacename.y,
-    "count.x"=merged$count.x,
-    "count.ynorm"=-merged$count.ynorm,
-    "count.y"=-merged$count.y
-  )
-
-  if (!display) pdf(file=paste(datafilename,"pdf",sep="."), width=15, height=12)
-  for (spacename in unique(data$spacename)) {
-    data_tmp=data[data$spacename==spacename,]
-
-
-    mypalette<-brewer.pal(3,"Set1")
-
-    gg = ggplot(data_tmp, aes(x=reorder(GO,-(count.x+count.ynorm)))) +
-      geom_bar(aes(y=count.x), position="identity",stat="identity", fill=mypalette[1], colour="black") +
-      geom_text(aes(y=count.x,label=count.x),vjust=-0.5, colour=mypalette[1], size=3.5) +
-      facet_grid(. ~ spacename, scales = "free", space = "free") +
-      theme_bw() +
-      theme(axis.text.x = element_text(angle=-90, hjust=0, vjust=0.5), plot.title = element_text(vjust=1.5), axis.title.x = element_blank(), axis.title.y = element_blank(), panel.grid.major = element_line(colour = "grey")) +
-      labs(title = datatitle)
-
-    # si second condition
-    if (sum(data$count.y) < 0) {
-      gg = gg + geom_bar(aes(y=count.y), position="identity",stat="identity", fill="white", colour="grey")
-      gg = gg + geom_bar(aes(y=count.ynorm), position="identity",stat="identity", fill=mypalette[3], colour="black")
-      gg = gg + geom_text(aes(y=count.y,label=(-1*count.y)),vjust=1.5, colour="grey", size=3.5)
-      gg = gg + geom_text(aes(y=count.ynorm,label=(-1*count.ynorm)),vjust=1.5, colour=mypalette[3], size=3.5)
-      gg = gg + expand_limits(y = c(min(data_tmp$count.y)-1,max(data_tmp$count.x)+1))
-    } else {
-      gg = gg + expand_limits(y = c(0,max(data_tmp$count.x)+1))
-    }
-
-    print(gg)
-  }
-  if (!display) dev.off()
+if (length(opt) > 0) {
+  display=F
+} else {  # if through RStudio for exemple
+  setwd(wd)
+  display=T
+  opt=list()
+  # Complete here
+  opt$file1 = NULL
+  opt$file2 = NULL
+  opt$shortname1 = NULL
+  opt$shortname2 = NULL
 }
+
+#display=T
+
+
+datafilename=paste(paste0(opt$shortname1,"_vs_",opt$shortname2),gsub("[[:space:]]", "", opt$other_info),sep=".")
+datatitle=paste(paste0(opt$shortname1," (red) vs ",opt$shortname2," (green-normed & white)\n"), opt$other_info,sep="")
+
+print(datafilename)
+
+cond1=read.table(opt$infile1); colnames(cond1) = c("GO","spacename","count")
+cond2=read.table(opt$infile2); colnames(cond2) = c("GO","spacename","count")
+
+# normalization on the number of GO in the cond1
+cond2$count.ynorm=round(cond2$count*opt$total1/opt$total2)
+
+
+merged=merge(cond1,cond2,by=c("GO","GO"),all=T)
+#merged=merged[merged$count.x!=0 & merged$countnorm!=0,] # pour les conditions vs whole
+
+merged$count.x[is.na(merged$count.x)]=0  # si absent de cond1
+merged$count.ynorm[is.na(merged$count.ynorm)]=0  # si absent de cond2
+merged$count.y[is.na(merged$count.y)]=0  # si absent de cond2
+merged$spacename.y[is.na(merged$spacename.y)]=merged$spacename.x[is.na(merged$spacename.y)] # si absent de cond1
+
+merged$chisq.obtained.x=merged$count.x
+merged$chisq.obtained.y=merged$count.y-merged$count.x
+merged$chisq.expected.x=merged$count.y*opt$total1/opt$total2
+merged$chisq.expected.y=merged$count.y-merged$chisq.expected.x
+merged$chisq.prop.x=merged$chisq.expected.x/merged$count.y
+merged$chisq.prop.y=merged$chisq.expected.y/merged$count.y
+head(merged)
+merged$khi2_pvalue=apply(cbind(merged$chisq.obtained.x,merged$chisq.obtained.y,merged$chisq.prop.x,merged$chisq.prop.y), 1, function(x) { chisq.test(cbind(x[1],x[2]),p=cbind(x[3],x[4]))$p.value })
+write.table(merged, file=paste(datafilename,"tab",sep="."))
+
+data=data.frame(
+  "GO"=merged$GO,
+  "spacename"=merged$spacename.y,
+  "count.x"=merged$count.x,
+  "count.ynorm"=-merged$count.ynorm,
+  "count.y"=-merged$count.y
+)
+
+if (!display) pdf(file=paste(datafilename,"pdf",sep="."), width=15, height=12)
+for (spacename in unique(data$spacename)) {
+  data_tmp=data[data$spacename==spacename,]
+
+
+  mypalette<-brewer.pal(3,"Set1")
+
+  gg = ggplot(data_tmp, aes(x=reorder(GO,-(count.x+count.ynorm)))) +
+    geom_bar(aes(y=count.x), position="identity",stat="identity", fill=mypalette[1], colour="black") +
+    geom_text(aes(y=count.x,label=count.x),vjust=-0.5, colour=mypalette[1], size=3.5) +
+    facet_grid(. ~ spacename, scales = "free", space = "free") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle=-90, hjust=0, vjust=0.5), plot.title = element_text(vjust=1.5), axis.title.x = element_blank(), axis.title.y = element_blank(), panel.grid.major = element_line(colour = "grey")) +
+    labs(title = datatitle)
+
+  # si second condition
+  if (sum(data$count.y) < 0) {
+    gg = gg + geom_bar(aes(y=count.y), position="identity",stat="identity", fill="white", colour="grey")
+    gg = gg + geom_bar(aes(y=count.ynorm), position="identity",stat="identity", fill=mypalette[3], colour="black")
+    gg = gg + geom_text(aes(y=count.y,label=(-1*count.y)),vjust=1.5, colour="grey", size=3.5)
+    gg = gg + geom_text(aes(y=count.ynorm,label=(-1*count.ynorm)),vjust=1.5, colour=mypalette[3], size=3.5)
+    gg = gg + expand_limits(y = c(min(data_tmp$count.y)-1,max(data_tmp$count.x)+1))
+  } else {
+    gg = gg + expand_limits(y = c(0,max(data_tmp$count.x)+1))
+  }
+
+  print(gg)
+}
+if (!display) dev.off()
 
 
 
